4de462a214c25576a2dba2685b8a4597e4952833
intent <zzy.main@gmail.com>
2020-06-19 20:28:07 +0800

    Java并发编程的艺术第四章第3节第一个小标题的技术验证.

---
+++ src/test/java/xyz/zzyitj/demo/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * xyz.zzyitj.demo
+ * <p>
+ * concurrent是多线程并发的一些测试
+ * concurrent.art是Java并发编程的艺术这本书的一些测试
+ *
+ * @author intent zzy.main@gmail.com
+ * @date 2020/6/19 7:54 下午
+ * @since 1.0
+ */
+package xyz.zzyitj.demo;
