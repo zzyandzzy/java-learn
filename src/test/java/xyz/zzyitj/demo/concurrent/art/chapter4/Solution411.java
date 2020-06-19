03c9e2cbc6b0409890c84f8a9d3d5cfbfa4068ff
intent <zzy.main@gmail.com>
2020-06-19 21:07:43 +0800

    Java并发编程的艺术4.1.1.

--- src/test/java/xyz/zzyitj/demo/concurrent/art/chapter4/Solution411.java
+++ src/test/java/xyz/zzyitj/demo/concurrent/art/chapter4/Solution411.java
@@ -1,4 +1,4 @@
-package xyz.zzyitj.demo.concurrent;
+package xyz.zzyitj.demo.concurrent.art.chapter4;
 
 import org.junit.Test;
 
@@ -8,12 +8,21 @@ import java.lang.management.ThreadMXBean;
 
 /**
  * xyz.zzyitj.demo.concurrent
+ * MultiThread
  *
  * @author intent zzy.main@gmail.com
  * @date 2020/6/12 11:30 上午
  * @since 1.0
  */
-public class MultiThreadTest {
+public class Solution411 {
+
+    /**
+     * 4-1
+     * [4] Signal Dispatcher 分发处理发送给JVM信号的线程
+     * [3] Finalizer 调用对象finalize方法的线程
+     * [2] Reference Handler 清除Reference的线程
+     * [1] main main线程，用户程序入口
+     */
     @Test
     public void test() {
         ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
